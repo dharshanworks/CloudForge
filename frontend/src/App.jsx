ffceb1e23f11d4c3300bef83b73d94bf3b6767e6
dharshanworks <dharshanworks@gmail.com>
2026-07-09 09:30:08 +0530

    chore(frontend): initialize react application frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div>
+        <h1>🚀 CloudForge</h1>
+
+        <p>Cloud Native Internal Developer Platform</p>
+
+        <br />
+
+        <small>Version 0.1.0</small>
+      </div>
+    </main>
+  );
+}
+
+export default App;
